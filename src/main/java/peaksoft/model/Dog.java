00a92d0e916efb30be0915d7266dfd3b6d80a2d2
SunflowerKun <dream26322gmail.com>
2022-05-14 19:59:36 +0600

    second init

---
+++ src/main/java/peaksoft/model/Dog.java
@@ -0,0 +1,14 @@
+package peaksoft.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("dogBean")
+public class Dog extends Animal{
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+
+}
